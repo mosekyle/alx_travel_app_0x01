25ef6e9067df7a78096d827517431c29c1fbf1ac
mosekyle <gitaumoseskiarie@gmail.com>
2025-01-17 19:25:53 +0300

    Update changes

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -1,16 +1,29 @@
+"""
+URL configuration for alx_travel_app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from listings import views  # Assuming you have views in the listings app
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# Swagger API Documentation view
 schema_view = get_schema_view(
    openapi.Info(
-      title="ALX Travel App API",
+      title="AlxTravelApp API",
       default_version='v1',
-      description="API documentation for the ALX Travel App",
+      description="API documentation for AlxTravelApp",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@alxtravelapp.local"),
       license=openapi.License(name="BSD License"),
@@ -18,13 +31,9 @@ schema_view = get_schema_view(
    public=True,
 )
 
-# Router for API views (assuming you have API views in the 'listings' app)
-router = routers.DefaultRouter()
-router.register(r'listings', views.ListingViewSet)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('swagger/', schema_view.as_view(), name='swagger-docs'),  # Swagger documentation URL
-    path('api/', include(router.urls)),  # Include API URLs
+    path('', include('listings.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
